624ddc366c0b994f83e86d69351c3215610bcf07
iwtba4188 <X69599596@gmail.com>
2025-05-25 01:03:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version="0.1.0",
     packages=["tangoflux"],
     install_requires=[
-        "torch==2.7.0+cu128",
-        "torchaudio==2.7.0+cu128",
+        "torch==2.7.0",
+        "torchaudio==2.7.0",
         "torchlibrosa==0.1.0",
         "torchvision",
         "transformers==4.44.0",
@@ -28,6 +28,6 @@ setup(
         ],
     },
     find_links=[
-        "https://download.pytorch.org/whl/cu128",
+        "https://download.pytorch.org/whl/cu126",
     ],
 )
